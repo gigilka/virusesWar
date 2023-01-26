a45b5c506b2bc3256fe8f81b6f516fefefe22c7a
gigilka <kuchin-02@mail.ru>
2023-01-26 22:42:25 +0300

    1

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const { Client } = require("pg");
 const { table } = require("console");
 gameStatus = "waiting";
 moves = 3;
-
+firstTurn = true;
 const client = new Client({
   user: "test",
   host: "localhost",
@@ -33,17 +33,135 @@ for (let i = 0; i < 10; i++) {
   tablee[i] = new Array(10).fill(0);
 }
 
+function findNeighbors(x, y) {
+  neighbors = [
+    { x: x, y: y - 1 },
+    { x: x, y: y + 1 },
+    { x: x - 1, y: y - 1 },
+    { x: x - 1, y: y },
+    { x: x - 1, y: y + 1 },
+    { x: x + 1, y: y - 1 },
+    { x: x + 1, y: y },
+    { x: x + 1, y: y + 1 },
+  ];
+  return neighbors;
+}
+
+function dfs(v) {
+  this.dmarked[v] = true;
+  console.log(v);
+  var temp = this.verticesRe[v];
+  for (var i = 0; i < temp.length; ++i) {
+    if (!this.dmarked[temp[i]]) {
+      this.dfs(temp[i]);
+    }
+  }
+}
+
+function bfs(v) {
+  var queue = [];
+  queue.push(v);
+  this.bmarked[v] = true;
+  while (queue.length) {
+    var shift = queue.shift(); // Удаляем первый элемент очереди
+    console.log(shift);
+    var temp = this.verticesRe[shift];
+    for (var i = 0; i < temp.length; ++i) {
+      if (!this.bmarked[temp[i]]) {
+        this.bmarked[temp[i]] = true;
+        queue.push(temp[i]);
+      }
+    }
+  }
+}
+
+function Graph(v) {
+  this.vertices = v; // количество вершин
+  this.edges = 0; // количество ребер
+  this.verticesRe = []; // Двумерный массив для хранения вершин, смежных с текущей вершиной
+  this.bmarked = []; // отмеченный массив
+  this.dmarked = []; // отмеченный массив
+  for (var i = 0; i < v; ++i) {
+    this.verticesRe[i] = [];
+    this.bmarked[i] = false;
+    this.dmarked[i] = false;
+  }
+  this.addEdge = addEdge;
+}
+
+this.dfs = dfs; // поиск в глубину
+this.bfs = bfs; // Поиск в ширину
+
+function addEdge(v, w) {
+  this.verticesRe[v].push(w);
+  this.verticesRe[w].push(v);
+  this.edges++;
+}
+
 function IsReachable(x, y, tablee, role) {
   neighbors = [
-    [x - 1, y - 1],
-    [x - 1, y],
-    [x - 1, y + 1],
-    [x, y + 1],
-    [x + 1, y + 1],
-    [x + 1, y],
-    [x + 1, y - 1],
-    [x, y - 1],
+    { x: x, y: y - 1 },
+    { x: x, y: y + 1 },
+    { x: x - 1, y: y - 1 },
+    { x: x - 1, y: y },
+    { x: x - 1, y: y + 1 },
+    { x: x + 1, y: y - 1 },
+    { x: x + 1, y: y },
+    { x: x + 1, y: y + 1 },
   ];
+
+  console.log(neighbors);
+
+  ends = [];
+  bases = [];
+  if (role == "bl") {
+    intRole = 1;
+    intChain = 2;
+  } else {
+    intRole = 3;
+    intChain = 4;
+  }
+
+  for (i = 0; i < neighbors.length; i++) {
+    if (tablee[neighbors[k].x][neighbors[k].y] == intRole) {
+      return true;
+    }
+  }
+
+  for (i = 0; i < neighbors.length; i++) {
+    if (tablee[neighbors[k].x][neighbors[k].y] == intChain) {
+      ends.push({ x: neighbors[k].x, y: neighbors[k].y });
+    }
+    if (ends.length == 0) {
+      return false;
+    }
+
+    let count = 0;
+
+    for (i = 0; i < 10; i++) {
+      for (j = 0; j < 10; j++) {
+        if (tablee[i][j] == intRole) {
+          ends.push({ x: i, y: j });
+          count++;
+        }
+        if (tablee[i][j] == intChain) {
+          count++;
+        }
+      }
+    }
+
+    g = new Graph(count);
+    for (i = 0; i < 10; i++) {
+      for (j = 0; j < 10; j++) {
+        if (tablee[i][j] == intRole || tablee[i][j] == intChain) {
+          nei = findNeighbors(i, j);
+          for (k = 0; k < nei.length; k++) {
+            
+          }
+        }
+      }
+    }
+  }
   console.log(neighbors);
   return true;
 }
